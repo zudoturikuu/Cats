15ee954e5182961717eb351033a41ab363d07cb8
Jun Heng <zudoturiku@duck.com>
2022-07-16 10:53:26 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Wong Jun heng on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
