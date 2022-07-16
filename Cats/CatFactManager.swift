a05cedcbb8f6fd469407cd16bbb5bfde2e5b7e27
Jun Heng <zudoturiku@duck.com>
2022-07-16 11:08:58 +0800

    Commit 3

---
+++ Cats/CatFactManager.swift
@@ -0,0 +1,21 @@
+//
+//  CatFactManager.swift
+//  Cats
+//
+//  Created by Wong Jun heng on 16/7/22.
+//
+
+import Foundation
+import SwiftUI
+class CatFactManager: ObservableObject {
+ func getCatFact() {
+ let apiURL = URL(string: "https://catfact.ninja/fact")!
+ let request = URLRequest(url: apiURL)
+
+ URLSession.shared.dataTask(with: request) { data, response, error in
+ if let data = data {
+ print(String(data: data, encoding: .utf8)!)
+ }
+ }.resume()
+ }
+}
--- Cats/CatFactView.swift
+++ Cats/CatFactView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct CatFactView: View {
+    @StateObject var catFactManager = CatFactManager()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear {
+                catFactManager.getCatFact()
+            }
     }
 }
 
